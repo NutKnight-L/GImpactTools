589f1f1e258bae2a31cfd2da3322eb21884f6dd6
NutKnight-L <2803793299@qq.com>
2022-07-24 16:34:57 +0800

    .

--- js/index.js
+++ js/index.js
@@ -1,15 +1,13 @@
-var btn = document.getElementById('btn');
-var ctr = document.getElementById('ctr');
-var a = 3;
-var b=0;
-btn.onclick = function(){
-    while(1){
-        ctr.innerHTML += "<div class='character'></div>";
-        b++;
-        // console.log("aa");
-        if(b == a){
-            break;
-        }
-    }
-    
+var ybtn = document.getElementById('ybtn');
+var nbtn = document.getElementById('nbtn');
+var main = document.getElementById('main');
+var ctrs = document.getElementsByClassName('container');
+var b = 0;
+ybtn.onclick = a; 
+function a() {
+    var pp = main.innerHTML;
+    main.innerHTML = pp + '<div class="container" id="ctr"><div class="character"></div><div class="character"></div><div class="character"></div></div>';
+}
+nbtn.onclick = function () {
+    main.innerHTML = '<div class="container" id="ctr"><div class="character"></div><div class="character"></div><div class="character"></div></div>';
 }
