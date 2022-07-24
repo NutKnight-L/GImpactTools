f9b6eefc2c26b5864047072955be736024b00568
NutKnight-L <2803793299@qq.com>
2022-07-24 19:18:58 +0800

    .

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 var ybtn = document.getElementById('ybtn');
 var nbtn = document.getElementById('nbtn');
+var abtn = document.getElementById('abtn');
+var hbtn = document.getElementById('hbtn');
 var main = document.getElementById('main');
 var ctrs = document.getElementsByClassName('container');
 var b = 2;
@@ -13,22 +15,43 @@ function a() {
     aa.appendChild(cc);
     cc.type = "button";
     cc.value = "x";
-    for (var i = 0; i < b; i++) {
-        var bb = document.createElement('div');
-        aa.appendChild(bb);
-        bb.className = 'character';
-    }
+    // for (var i = 0; i < b; i++) {
+    //     var bb = document.createElement('div');
+    //     aa.appendChild(bb);
+    //     bb.className = 'character';
+    // }
     nu++;
-    cc.id='btn'+nu;
-    aa.id=nu;
-    var dbtn = document.getElementById('btn'+nu);
-    dbtn.onclick = function(){
+    cc.id = 'btn' + nu;
+    aa.id = nu;
+    var dbtn = document.getElementById('btn' + nu);
+    dbtn.onclick = function () {
         // alert(cc.id);
         main.removeChild(aa);
         nu--;
     }
     main.focus();
 }
-nbtn.onclick = function () {
-    alert("hh");
+abtn.onclick = function () {
+    var intH = document.documentElement.scrollHeight;
+    var b = document.createElement('div');
+    var a = document.createElement('div');
+
+    document.body.appendChild(b);
+    document.body.appendChild(a);
+
+    a.className = 'cover';
+    b.className = 'newWindow';
+    a.style.height = intH + 'px';
+    console.log(intH);
+    var bbtn = document.createElement('input');
+    b.appendChild(bbtn);
+    bbtn.type = 'button';
+    bbtn.value = 'close the window';
+    bbtn.onclick = function () {
+        a.style.display = 'none';
+        b.style.display = 'none';
+    }
+}
+hbtn.onclick = function(){
+    console.log(document.documentElement.scrollHeight);
 }
